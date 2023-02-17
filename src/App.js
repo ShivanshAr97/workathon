f1148cf24268aff09c3494c3ed82eaa4a47139cd
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-02-18 01:37:48 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <div className='bg-blue-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit culpa vitae, odio at molestias quam magni tempore tempora eius libero id. Aliquam deserunt soluta, magnam ratione perferendis quos beatae voluptate.</div>
+    </>
+  );
+}
+
+export default App;

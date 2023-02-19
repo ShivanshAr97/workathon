b20915859b0152b0b5128687d993350eaa0cb21a
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-02-19 15:01:52 +0530

    commit

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import First from './First'
-import Second from './Second'
 
 const Games = () => {
   return (
@@ -8,7 +7,6 @@ const Games = () => {
     <h1 className='flex justify-center text-4xl font-semibold mt-8 mb-2'>Games</h1>
     <h2 className='flex justify-center text-xl'>Lighten up your mood with some easy and simple yet addictable games</h2>
     <First/>
-    <Second/>
     </>
   )
 }

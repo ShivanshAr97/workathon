cac8790bb77bdd8f6fce0153b1932338cd242346
ShivanshAr97 <96040322+ShivanshAr97@users.noreply.github.com>
2023-02-19 16:53:34 +0530

    completed

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
                 <img className='mx-12 my-24 rounded-lg' height="360px" width="500" src="https://images.pexels.com/photos/3777554/pexels-photo-3777554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
                 <div className='mr-12'>
                     <h1 className='text-4xl font-bold mb-2'>Tired of the work at office? 😵</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio alias neque rerum ea officiis in, fugit maiores error quaerat deserunt recusandae reprehenderit qui tempore magni provident tenetur pariatur, consequatur, ad quod velit inventore esse? Quos, totam! Quo porro temporibus eveniet, laboriosam nobis architecto ratione, ut ipsa quasi suscipit iure quidem modi corporis ab aut, ipsam tenetur alias fugit omnis eligendi. Minus quia totam quas, numquam eos et quisquam itaque corporis ab placeat assumenda vero. Possimus quidem recusandae qui? Magni quod iste aliquam praesentium animi officia architecto dolorum, neque, ex, blanditiis enim. Sequi, dolore quasi eaque provident a iure praesentium omnis.</p>
+                    <p>According to a study, more than 60% of the people are tired of the work in their office. They struggle to focus on it or eventually lose interest in it. They become less motivated to do the task. They become drawn to the things we want to do like scrolling Instagram or reading music blogs rather than the things they have to do. And this tension possibly causes fatigue and stress.</p>
                 </div>
             </div>
             <div className='bg-blue-400 p-4'>
@@ -19,11 +19,11 @@ const Main = () => {
                 <div>
                     <h1 className='text-2xl font-semibold m-2'>At Cubicle Carnival, we provide: </h1>
                     <ul>
-                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Games to chill out</span></li>
-                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Jokes</span></li>
-                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Music to calm done the nerves</span></li>
-                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Upskilling self</span></li>
-                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Celebrating connections, wishing others to make a sense of livelihood</span></li>
+                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Fun games to chill out</span></li>
+                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Jokes to reduce stress</span></li>
+                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Music that calm done the nerves</span></li>
+                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Upskilling yourself & staying ahead of others</span></li>
+                        <li className='flex items-center'><BiCheck/><span className='mx-2 text-lg'>Celebrating & wishing connections to make a sense of livelihood</span></li>
                     </ul>
                 </div>
                 <img className='rounded-lg mr-12' height="400" width="500" src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,7 @@ const Skills = () => {
       <div className='flex mx-8 items-center'>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>1. Read a book or listen to a book summary</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>Good books can inform you, enlighten you and lead you in the right direction. There is no better companion than a good book. Reading is important because it is good for your overall well-being. Once you start reading, you experience a whole new world. Reading develops language skills and vocabulary. Reading books is also a way to relax and reduce stress.</p>
         </div>
         <div className='w-[50%] flex justify-center'>
           <iframe className='rounded-lg m-4' width="350" height="200" src="https://www.youtube.com/embed/eDa1U9qJKxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -21,14 +21,14 @@ const Skills = () => {
         </div>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>2. Learn a new language</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>Learning languages is an important part of being very soft and friendly welcomed in different countries. Even only one language cans totally change the way of the communication, the attitude towards you and the impression which you will make in people with which you conduct a conversation</p>
         </div>
       </div>
 
       <div className='flex mx-8 items-center'>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>3. Try hands on cooking</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>cooking skills may help people to meet nutrition guidelines in their daily nutrition supply. They allow people to make healthier food choices. It is, therefore, important to teach children and teenagers how to cook and to encourage them to develop their cooking skills.</p>
         </div>
         <div className='w-[50%] flex justify-center'>
           <iframe className='rounded-lg m-4' width="350" height="200" src="https://www.youtube.com/embed/FTociictyyE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -42,14 +42,14 @@ const Skills = () => {
         </div>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>4. Learn the most trending skills of the time</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>By keeping up with trends, you'll constantly be learning; new tools, new events to participate in, new ways to be efficient. This will give you a wider variety of things to work with- therefore, you will be able to experiment with more things and use them to generate new ideas.</p>
         </div>
       </div>
 
       <div className='flex mx-8 items-center'>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>5. Explore universe and learn astronomy</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>Always having an unexplained mystery, astronomy also has served as a way to keep time and predict the future. The word “astronomy” is defined as the study of heavenly bodies, meaning anything in the sky such as stars, galaxies, comets, planets, nebulae, and so on.</p>
         </div>
         <div className='w-[50%] flex justify-center'>
           <iframe className='rounded-lg m-4' width="350" height="200" src="https://www.youtube.com/embed/8vkqH3r1-f4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -62,7 +62,7 @@ const Skills = () => {
         </div>
         <div className='w-[50%]'>
           <h1 className='text-lg font-medium'>6. Improve memory</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat deserunt natus qui expedita illo, sint pariatur explicabo commodi at vel repellat, similique dolorem corrupti quo delectus mollitia est nulla. Nesciunt nemo nihil necessitatibus autem sequi! Debitis molestiae alias esse, quasi aspernatur consequatur nesciunt officiis quod vitae libero, rerum eveniet?</p>
+          <p>Memory is a very important tool to our everyday lives.  Without memory, we would not be able to function very productively.  You can look at someone suffering with Alzheimer’s disease and see the effects of the inability to make new memories, as well as lost and confused memory.</p>
         </div>
       </div>
     </>
